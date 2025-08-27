4497663c65a58cf1e7ee0e2b7ae83034acbe30aa
to-higashiguchi <to-higashiguchi@kddi-agdc.com>
2025-08-27 22:51:56 +0900

    環境変数もデプロイできるように

--- hello_lambda/app.py
+++ hello_lambda/app.py
@@ -1,12 +1,21 @@
 import json
+import os
 
 
 def lambda_handler(event, context):
+    # 環境変数を取得
+    environment = os.environ.get("ENVIRONMENT", "development")
+    log_level = os.environ.get("LOG_LEVEL", "debug")
+    app_name = os.environ.get("APP_NAME", "hello_lambda")
+
     return {
         "statusCode": 200,
         "body": json.dumps(
             {
                 "message": "hello_world",
+                "environment": environment,
+                "log_level": log_level,
+                "app_name": app_name,
             }
         ),
     }
