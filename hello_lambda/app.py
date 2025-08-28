ac2ee919e77d9e1b537689301ae7397564fdd676
to-higashiguchi <to-higashiguchi@kddi-agdc.com>
2025-08-28 23:25:21 +0900

    ローカルテストができるようになった

--- hello_lambda/app.py
+++ hello_lambda/app.py
@@ -1,21 +1,37 @@
 import json
-import os
 
 
 def lambda_handler(event, context):
-    # 環境変数を取得
-    environment = os.environ.get("ENVIRONMENT")
-    log_level = os.environ.get("LOG_LEVEL")
-    app_name = os.environ.get("APP_NAME")
-
+    """
+    AWS Lambdaで実行されるメインの関数。
+    """
     return {
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello_hoge",
-                "environment": environment,
-                "log_level": log_level,
-                "app_name": app_name,
+                "message": "hello_lambda!",
             }
         ),
     }
+
+
+def main():
+    """
+    ローカルでのテスト実行用のメイン関数。
+    """
+    # Lambda関数に渡すeventとcontextのダミーデータを作成
+    # event: Lambda関数への入力データ。API Gatewayからのリクエストなどを想定
+    mock_event = {}
+
+    # context: Lambdaの実行環境に関する情報。通常、ローカルテストでは多くを必要としない
+    mock_context = {}
+
+    # lambda_handlerを直接呼び出し、結果を表示
+    response = lambda_handler(mock_event, mock_context)
+    print("Lambda function response:")
+    print(json.dumps(response, indent=4))
+
+
+# スクリプトが直接実行された場合にmain()を呼び出す
+if __name__ == "__main__":
+    main()
