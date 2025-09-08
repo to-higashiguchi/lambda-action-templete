38190060b30147295ec9ba9b426868f00c99468a
to-higashiguchi <to-higashiguchi@kddi-agdc.com>
2025-09-08 13:36:57 +0900

    tsでもできるかな

---
+++ hello_lambda_ts/src/index.ts
@@ -0,0 +1,11 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      message: 'hello from TypeScript!',
+    }),
+  };
+};
